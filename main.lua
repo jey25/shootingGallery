d00b3543ac0efb0ea930cce3fa2f53606311777c
jey25 <jjangeoyoung@gmail.com>
2023-01-24 23:15:19 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,11 @@
 function love.load()
-    number = 0
+
 end
 
-
-
 function love.update(dt)
-    number = number + 1
+
 end
 
 function love.draw()
-    love.graphics.print(number)
+    love.graphics.rectangle("fill", 0, 0, 200, 100)
 end
