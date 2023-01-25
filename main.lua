93d8205dea5d3506edf2df64df7cc060b8928a60
jey25 <jjangeoyoung@gmail.com>
2023-01-25 23:17:52 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,5 +7,7 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", 0, 0, 200, 100)
+    love.graphics.rectangle("fill", 200, 400, 200, 100)
+    love.graphics.circle("fill", 300, 200, 100)
 end
+ 
