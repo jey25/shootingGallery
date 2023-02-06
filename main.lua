480bf4a07e5c088fe06f1c6d362cb08936915760
jey25 <jjangeoyoung@gmail.com>
2023-02-07 00:05:23 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,11 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setColor(1, 1, 0)
     love.graphics.rectangle("fill", 200, 400, 200, 100)
+    
+    
+    love.graphics.setColor(0, 1, 0)
     love.graphics.circle("fill", 300, 200, 100)
 end
  
