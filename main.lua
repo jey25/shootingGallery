55336b72e6b4b162108874bf7b4d83d4875eb762
jey25 <jjangeoyoung@gmail.com>
2023-02-07 00:08:03 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ function love.draw()
     love.graphics.rectangle("fill", 200, 400, 200, 100)
     
     
-    love.graphics.setColor(0, 1, 0)
+    love.graphics.setColor(204/255, 102/255, 255/255)
     love.graphics.circle("fill", 300, 200, 100)
 end
  
