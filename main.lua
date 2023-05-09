e1fb03bb25f435dc74417cf4e8352dd18e8359f8
jey25 <jjangeoyoung@gmail.com>
2023-05-10 00:05:42 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ function love.load()
 
     score = 0
     timer = 0
+
+    gameFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -15,5 +17,22 @@ end
 function love.draw()
     love.graphics.setColor(1, 0, 0)
     love.graphics.circle("fill", target.x, target.y, target.radius)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print(score, 50, 50)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 then
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+        if mouseToTarget < target.radius then
+            score = score + 1
+        end
+    end
+end
+
+function distanceBetween(x1 , y1 , x2 , y2)
+    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end
  
