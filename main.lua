5885a08215427df6f65d262bd07ba2a1cd5db8b5
jey25 <jjangeoyoung@gmail.com>
2023-05-17 00:06:36 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -114,3 +114,14 @@ function module:TargetPlayer(distance)
 end
 
 return module
+
+
+--StarterplayerScript > Localscript 로 캐릭터 컨트롤 On/off 조작
+
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+local module = require(player:WaitForChild("PlayerScripts").PlayerModule)
+local control = module:GetControls()
+
+--control:Disable() -- 컨트롤 권한 끄기
+--control:Enable() -- 컨트롤 권한 켜기
